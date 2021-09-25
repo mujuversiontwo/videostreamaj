2b86235c9e7126a55807345627f037bccb53a223
mujuversiontwo <89786887+mujuversiontwo@users.noreply.github.com>
2021-09-25 14:28:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/mujuversiontwo/videostreamaj
+git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
