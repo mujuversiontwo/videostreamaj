1eb7f10f024e8ec4ef631d06ff9b0713d1440b83
mujuversiontwo <89786887+mujuversiontwo@users.noreply.github.com>
2021-09-25 13:39:55 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -97,8 +97,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/VCPlayerBot'),
+                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/worldmoviesaj'),
+                InlineKeyboardButton('🧩 Source', url='https://t.me/worldmoviesaj'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
